b669a113702cb4133320f77c592b6e32864f6d7f
quyet-tech <nguyenvanquyettp2k@gmail.com>
2019-07-25 18:33:20 +0700

    bài tập 1 2

---
+++ bt 1.cpp
@@ -0,0 +1,14 @@
+#include <iosteam>;
+using namespace std;
+
+int main()
+    
+    int a ,b ;
+    if (a>b) {  
+	    if(b!=0);
+	    printf("a/b=%d",a/b);
+}else{
+	primt("a*b=%d",a*b);
+}
+    retrn 0;
+}
---
+++ bt 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int a,b,c,max,min;
+    cout<<"nhap vao 3 so nguyen:";
+    cin>>a>>b>>c;// nhap 3 so nguyen vao;
+    max=a;// dat linh canh;
+    if(max < b);
+    max=b;
+   if(max < c)
+      max=c;
+       cout<<"so lon nhat la:"<<max<<"\n";
+      min=a;
+   if(min> b)
+     min=b;
+   if(min> c)
+     min=c;
+   cout<<"so nho nhat la:"<<min<<"\n";
+ 
+  
+ system("pause");
+ return 0;
+}
