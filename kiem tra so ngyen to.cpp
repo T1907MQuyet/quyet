777eb34ccd300ce0c6e0dcfe47bbe341b027a0a2
quyet-tech <nguyenvanquyettp2k@gmail.com>
2019-08-02 06:23:01 +0700

    quyet

--- kiem tra so ngyen to.cpp
+++ kiem tra so ngyen to.cpp
@@ -6,26 +6,19 @@ int main(){
     int n;
     printf("\nNhap n =/n ");
     scanf("%d", &n);
-    if(n < 2){
-        printf("\n%d khong phai so nguyen to", n);
-        return 0;
-    }
+    for (int i =1 ; i<=n ; ++i)
+    {
     int count = 0;
-    for(int i = 2; i <=n; i++){
-        if(n % i == 0){
+    for(int j = 1; j <=i; j++){
+        if(i % j == 0){
             count++;
-            
-        }
+            }
+    if(count == 2){
+        printf("\n%d la so nguyen to nho hon n ", i);
+    
     }
-    if(count == 0){
-        printf("\n%d la so nguyen to", n);
-    }else{
-        printf("\n%d khong phai so nguyen to", n);
-        
-		
-		return 0;
-		
-		
-        
-        }
-   }
+    }
+    return 0;
+    }
+    
+    
