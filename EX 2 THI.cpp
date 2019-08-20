6b1c4f042592bd8fb41dc792d5767ec39ec371d1
quyet-tech <nguyenvanquyettp2k@gmail.com>
2019-08-20 22:58:28 +0700

    quyet

--- EX 2 THI.cpp
+++ EX 2 THI.cpp
@@ -4,12 +4,15 @@
 int main (){
     int a[10];
     for (int i=1;i<10;i++)
-    { 
+    
+    { int sum;
     printf("enter telement [%d]:",i);
     scanf("%d",&a[i]);
     }
-    for (int i=10;i>0;i--)
+    for (int i=;i>=0;i--)
+    
     {
+    
     printf ("%5d",a[i]);
     }
     return 0;
